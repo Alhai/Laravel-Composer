7a0da19868ed31a3cac0c476b7e5a0e06bc261d8
aly <alhai38.icloud.com>
2022-03-11 17:00:38 +0100

    "fiche produits"

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,13 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
-    public function Product(){
-        return view('product-list');
+    public function productbdd()
+    {
+        $products = DB::select('select * from products');
+
+        return view('product-list',['products'=>$products]);
+
     }
+    // public function Product(){
+    //     return view('product-list');
+    // }
     public function productId($id){
-        return view('product-details',['id' => $id]);
+        $products = DB::select('select * from products where id = :id', array('id' => $id));
+        return view('product-details',['id' => $id],['products'=>$products]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CartController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,5 +19,8 @@ use App\Http\Controllers\CartController;
 
 Route::get('/',[HomeController::class,'Home']);
 Route::get('/product',[ProductController::class,'Product']);
+Route::get('/product',[ProductController::class,'productBdd']);
+Route::get('/product/{id}',[ProductController::class,'productBdd']);
 Route::get('/cart',[CartController::class,'cart']);
 Route::get('/product/{id}', [ProductController::class,'productId']);
+
